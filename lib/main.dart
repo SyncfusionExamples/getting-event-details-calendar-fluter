13f88ae4a3c1cc58da97443c3b4ede05d9175af6
Indumathi1195R <54974849+Indumathi1195R@users.noreply.github.com>
2021-04-14 07:40:13 +0530

    Update main.dart
    Sample updated.

--- lib/main.dart
+++ lib/main.dart
@@ -28,125 +28,80 @@ List<String> colors = <String>[
   'Default'
 ];
 
-List<String> views = <String>[
-  'Day',
-  'Week',
-  'WorkWeek',
-  'Month',
-  'Timeline Day',
-  'Timeline Week',
-  'Timeline WorkWeek'
-];
-
 class ScheduleExample extends State<TapAppointmentDetails> {
-  CalendarView _calendarView=CalendarView.week;
-  String? _subjectText, _startTimeText, _endTimeText,
-      _dateText, _timeDetails;
-  Color? headerColor, viewHeaderColor, calendarColor, defaultColor;
-
-  @override
-  void initState() {
-
-    _subjectText = '';
-    _startTimeText = '';
-    _endTimeText = '';
-    _dateText = '';
-    _timeDetails = '';
-    super.initState();
-  }
+  String? _subjectText = '',
+      _startTimeText = '',
+      _endTimeText = '',
+      _dateText = '',
+      _timeDetails = '';
+  Color? _headerColor, _viewHeaderColor, _calendarColor;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            PopupMenuButton<String>(
-              icon: Icon(Icons.color_lens),
-              itemBuilder: (BuildContext context) {
-                return colors.map((String choice) {
-                  return PopupMenuItem<String>(
-                    value: choice,
-                    child: Text(choice),
-                  );
-                }).toList();
-              },
-              onSelected: (String value) {
-                setState(() {
-                  if (value == 'Pink') {
-                    headerColor = const Color(0xFF09e8189);
-                    viewHeaderColor = const Color(0xFF0f3acb6);
-                    calendarColor = const Color(0xFF0ffe5d8);
-                  } else if (value == 'Blue') {
-                    headerColor = const Color(0xFF0007eff);
-                    viewHeaderColor = const Color(0xFF03aa4f6);
-                    calendarColor = const Color(0xFF0bae5ff);
-                  } else if (value == 'Wall Brown') {
-                    headerColor = const Color(0xFF0937c5d);
-                    viewHeaderColor = const Color(0xFF0e6d9b1);
-                    calendarColor = const Color(0xFF0d1d2d6);
-                  } else if (value == 'Yellow') {
-                    headerColor = const Color(0xFF0f7ed53);
-                    viewHeaderColor = const Color(0xFF0fff77f);
-                    calendarColor = const Color(0xFF0f7f2cc);
-                  } else if (value == 'Default') {
-                    headerColor = null;
-                    viewHeaderColor = null;
-                    calendarColor = null;
-                  }
-                });
-              },
-            ),
-          ],
-          backgroundColor: headerColor,
-          centerTitle: true,
-          titleSpacing: 60,
-          leading: PopupMenuButton<String>(
-            icon: Icon(Icons.calendar_today),
-            itemBuilder: (BuildContext context) => views.map((String choice) {
-              return PopupMenuItem<String>(
-                value: choice,
-                child: Text(choice),
-              );
-            }).toList(),
-            onSelected: (String value) {
-              setState(() {
-                if (value == 'Day') {
-                  _calendarView = CalendarView.day;
-                } else if (value == 'Week') {
-                  _calendarView = CalendarView.week;
-                } else if (value == 'WorkWeek') {
-                  _calendarView = CalendarView.workWeek;
-                } else if (value == 'Month') {
-                  _calendarView = CalendarView.month;
-                } else if (value == 'Timeline Day') {
-                  _calendarView = CalendarView.timelineDay;
-                } else if (value == 'Timeline Week') {
-                  _calendarView = CalendarView.timelineWeek;
-                } else if (value == 'Timeline WorkWeek') {
-                  _calendarView = CalendarView.timelineWorkWeek;
-                }
-              });
-            },
-          ),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: SfCalendar(
-                viewHeaderStyle:
-                ViewHeaderStyle(backgroundColor: viewHeaderColor),
-                backgroundColor: calendarColor,
-                view: _calendarView,
-                monthViewSettings: MonthViewSettings(showAgenda: true),
-                dataSource: getCalendarDataSource(),
-                onTap: calendarTapped,
+          appBar: AppBar(
+            actions: <Widget>[
+              PopupMenuButton<String>(
+                icon: Icon(Icons.color_lens),
+                itemBuilder: (BuildContext context) {
+                  return colors.map((String choice) {
+                    return PopupMenuItem<String>(
+                      value: choice,
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+                onSelected: (String value) {
+                  setState(() {
+                    if (value == 'Pink') {
+                      _headerColor = const Color(0xFF09e8189);
+                      _viewHeaderColor = const Color(0xFF0f3acb6);
+                      _calendarColor = const Color(0xFF0ffe5d8);
+                    } else if (value == 'Blue') {
+                      _headerColor = const Color(0xFF0007eff);
+                      _viewHeaderColor = const Color(0xFF03aa4f6);
+                      _calendarColor = const Color(0xFF0bae5ff);
+                    } else if (value == 'Wall Brown') {
+                      _headerColor = const Color(0xFF0937c5d);
+                      _viewHeaderColor = const Color(0xFF0e6d9b1);
+                      _calendarColor = const Color(0xFF0d1d2d6);
+                    } else if (value == 'Yellow') {
+                      _headerColor = const Color(0xFF0f7ed53);
+                      _viewHeaderColor = const Color(0xFF0fff77f);
+                      _calendarColor = const Color(0xFF0f7f2cc);
+                    } else if (value == 'Default') {
+                      _headerColor = null;
+                      _viewHeaderColor = null;
+                      _calendarColor = null;
+                    }
+                  });
+                },
               ),
+            ],
+            backgroundColor: _headerColor,
+          ),
+          body: SafeArea(
+            child: SfCalendar(
+              viewHeaderStyle:
+                  ViewHeaderStyle(backgroundColor: _viewHeaderColor),
+              backgroundColor: _calendarColor,
+              view: CalendarView.week,
+              allowedViews: [
+                CalendarView.day,
+                CalendarView.week,
+                CalendarView.workWeek,
+                CalendarView.month,
+                CalendarView.timelineDay,
+                CalendarView.timelineWeek,
+                CalendarView.timelineWorkWeek,
+              ],
+              monthViewSettings: MonthViewSettings(showAgenda: true),
+              dataSource: getCalendarDataSource(),
+              onTap: calendarTapped,
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 
